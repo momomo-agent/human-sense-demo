1fef3dbb8ac61a05e888f8b6d6d011df6da09dd9
momomo-agent <momomo-agent@users.noreply.github.com>
2026-04-08 20:42:19 +0800

    fix: move Emotion enum to separate file to fix import issues

--- Sources/Models/FaceState.swift
+++ Sources/Models/FaceState.swift
@@ -1,16 +1,6 @@
 import Foundation
 import ARKit
 
-// Import Emotion from EmotionDetector
-enum Emotion: String {
-    case neutral = "😐 中性"
-    case happy = "😊 开心"
-    case sad = "😢 悲伤"
-    case surprised = "😮 惊讶"
-    case angry = "😠 愤怒"
-    case disgusted = "🤢 厌恶"
-}
-
 struct FaceState {
     // Gaze
     var gazePoint: CGPoint = .zero
